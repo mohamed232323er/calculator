790cea90bd3035eca57e7b31db1aded374018d90
Mohamed Saeed Abdulatif <engmohamedsaeedid172@gmail.com>
2026-09-12 20:53:24 +0300

    add addition feature solution

---
+++ Calculator_solution/Addition Feature/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = Add(num1, num2);
+
+        Console.WriteLine($"Result: {result}");
+    }
+
+    static double Add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+}
