c20d8f5296f22c39a24f925983d2666091dafcb8
Mohamed Saeed Abdulatif <engmohamedsaeedid172@gmail.com>
2026-09-12 23:29:36 +0300

    add continuous calculation feature

---
+++ Calculator_solution/continuous-calculation-feature/Program5.cs
@@ -0,0 +1,12 @@
+﻿bool continueCalculating = true;
+
+while (continueCalculating)
+{
+    // ... do a calculation ...
+
+    Console.Write("Do you want to perform another calculation? (y/n): ");
+    string? answer = Console.ReadLine();
+    continueCalculating = answer != null && answer.ToLower() == "y";
+}
+
+Console.WriteLine("Goodbye!");
